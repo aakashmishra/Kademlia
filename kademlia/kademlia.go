7bc5b56c1720ba46e90f9121a25d777aad3007a7
uri klarman <uri.klarman@gmail.com>
2015-04-27 15:25:41 -0500

    testing enablement

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -80,36 +80,36 @@ func NewKademlia(laddr string) *Kademlia {
 	// }
 	// time.Sleep(1 * time.Second)
 
-	// for i := 0; i < numContacts; i++ {
-	// 	found, err := kadem.FindContact(contacts[i].NodeID)
-	// 	log.Printf("found : %v err: %v", found, err)
-	// }
+	for i := 0; i < numContacts; i++ {
+		found, err := kadem.FindContact(contacts[i].NodeID)
+		log.Printf("found : %v err: %v", found, err)
+	}
 
-	contact_1 := Contact{NewRandomID(), host, uint16(1)}
-	contact_2 := Contact{NewRandomID(), host, uint16(2)}
-	contact_3 := Contact{NewRandomID(), host, uint16(3)}
-	contact_4 := Contact{NewRandomID(), host, uint16(4)}
-	contact_4.NodeID[19] = kadem.SelfContact.NodeID[19]
-	contact_4.NodeID[18] = kadem.SelfContact.NodeID[18]
-	contact_5 := Contact{NewRandomID(), host, uint16(port_int)}
+	// contact_1 := Contact{NewRandomID(), host, uint16(1)}
+	// contact_2 := Contact{NewRandomID(), host, uint16(2)}
+	// contact_3 := Contact{NewRandomID(), host, uint16(3)}
+	// contact_4 := Contact{NewRandomID(), host, uint16(4)}
+	// contact_4.NodeID[19] = kadem.SelfContact.NodeID[19]
+	// contact_4.NodeID[18] = kadem.SelfContact.NodeID[18]
+	// contact_5 := Contact{NewRandomID(), host, uint16(port_int)}
 
-	log.Printf("contact_1 : %v", contact_1)
-	log.Printf("contact_2 : %v", contact_2)
-	log.Printf("contact_3 : %v", contact_3)
-	log.Printf("contact_4 : %v", contact_4)
-	log.Printf("contact_5 : %v", contact_5)
+	// log.Printf("contact_1 : %v", contact_1)
+	// log.Printf("contact_2 : %v", contact_2)
+	// log.Printf("contact_3 : %v", contact_3)
+	// log.Printf("contact_4 : %v", contact_4)
+	// log.Printf("contact_5 : %v", contact_5)
 
-	log.Printf("contact_1 distance: %v", kadem.Kbs.selfID.Xor(contact_1.NodeID))
-	log.Printf("contact_2 distance: %v", kadem.Kbs.selfID.Xor(contact_2.NodeID))
-	log.Printf("contact_3 distance: %v", kadem.Kbs.selfID.Xor(contact_3.NodeID))
-	log.Printf("contact_4 distance: %v", kadem.Kbs.selfID.Xor(contact_4.NodeID))
+	// log.Printf("contact_1 distance: %v", kadem.Kbs.selfID.Xor(contact_1.NodeID))
+	// log.Printf("contact_2 distance: %v", kadem.Kbs.selfID.Xor(contact_2.NodeID))
+	// log.Printf("contact_3 distance: %v", kadem.Kbs.selfID.Xor(contact_3.NodeID))
+	// log.Printf("contact_4 distance: %v", kadem.Kbs.selfID.Xor(contact_4.NodeID))
 
-	kadem.Kbs.Update(contact_1)
-	kadem.Kbs.Update(contact_2)
-	kadem.Kbs.Update(contact_3)
-	kadem.Kbs.Update(contact_4)
+	// kadem.Kbs.Update(contact_1)
+	// kadem.Kbs.Update(contact_2)
+	// kadem.Kbs.Update(contact_3)
+	// kadem.Kbs.Update(contact_4)
 
-	// time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	// log.Printf("kadem.Kbs.buckets[158] : %v", kadem.Kbs.buckets[158])
 	// log.Printf("kadem.Kbs.buckets[159] : %v", kadem.Kbs.buckets[159])
